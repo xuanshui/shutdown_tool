b4b80f326c5f95bf6006ef5573a3c6621c640e5d
xuanshui <xuanshui@outlook.com>
2023-02-06 21:38:57 +0800

    添加项目文件。

---
+++ shutdown_tool/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace shutdown_tool
+{
+    partial class ShutdownTool
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShutdownTool));
+            this.btn_sleep = new System.Windows.Forms.Button();
+            this.btn_shutdown = new System.Windows.Forms.Button();
+            this.btn_reload = new System.Windows.Forms.Button();
+            this.btn_sleep_count_down = new System.Windows.Forms.Button();
+            this.btn_shutdown_count_down = new System.Windows.Forms.Button();
+            this.btn_reload_count_down = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox_select_time = new System.Windows.Forms.ComboBox();
+            this.btn_sleep_1 = new System.Windows.Forms.Button();
+            this.btn_sleep_1_count_down = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_sleep
+            // 
+            this.btn_sleep.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_sleep.Location = new System.Drawing.Point(71, 133);
+            this.btn_sleep.Name = "btn_sleep";
+            this.btn_sleep.Size = new System.Drawing.Size(130, 60);
+            this.btn_sleep.TabIndex = 0;
+            this.btn_sleep.Text = "休眠";
+            this.btn_sleep.UseVisualStyleBackColor = true;
+            this.btn_sleep.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_shutdown
+            // 
+            this.btn_shutdown.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_shutdown.Location = new System.Drawing.Point(71, 280);
+            this.btn_shutdown.Name = "btn_shutdown";
+            this.btn_shutdown.Size = new System.Drawing.Size(120, 50);
+            this.btn_shutdown.TabIndex = 1;
+            this.btn_shutdown.Text = "关机";
+            this.btn_shutdown.UseVisualStyleBackColor = true;
+            this.btn_shutdown.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_reload
+            // 
+            this.btn_reload.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_reload.Location = new System.Drawing.Point(71, 428);
+            this.btn_reload.Name = "btn_reload";
+            this.btn_reload.Size = new System.Drawing.Size(120, 50);
+            this.btn_reload.TabIndex = 2;
+            this.btn_reload.Text = "重启";
+            this.btn_reload.UseVisualStyleBackColor = true;
+            this.btn_reload.Click += new System.EventHandler(this.btn_reload_Click);
+            // 
+            // btn_sleep_count_down
+            // 
+            this.btn_sleep_count_down.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_sleep_count_down.Location = new System.Drawing.Point(264, 133);
+            this.btn_sleep_count_down.Name = "btn_sleep_count_down";
+            this.btn_sleep_count_down.Size = new System.Drawing.Size(210, 50);
+            this.btn_sleep_count_down.TabIndex = 3;
+            this.btn_sleep_count_down.Text = "倒计时休眠";
+            this.btn_sleep_count_down.UseVisualStyleBackColor = true;
+            this.btn_sleep_count_down.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn_shutdown_count_down
+            // 
+            this.btn_shutdown_count_down.Font = new System.Drawing.Font("黑体", 10.5F);
+            this.btn_shutdown_count_down.Location = new System.Drawing.Point(264, 280);
+            this.btn_shutdown_count_down.Name = "btn_shutdown_count_down";
+            this.btn_shutdown_count_down.Size = new System.Drawing.Size(210, 50);
+            this.btn_shutdown_count_down.TabIndex = 4;
+            this.btn_shutdown_count_down.Text = "倒计时关机";
+            this.btn_shutdown_count_down.UseVisualStyleBackColor = true;
+            this.btn_shutdown_count_down.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn_reload_count_down
+            // 
+            this.btn_reload_count_down.Font = new System.Drawing.Font("黑体", 10.5F);
+            this.btn_reload_count_down.Location = new System.Drawing.Point(264, 428);
+            this.btn_reload_count_down.Name = "btn_reload_count_down";
+            this.btn_reload_count_down.Size = new System.Drawing.Size(210, 50);
+            this.btn_reload_count_down.TabIndex = 5;
+            this.btn_reload_count_down.Text = "倒计时重启";
+            this.btn_reload_count_down.UseVisualStyleBackColor = true;
+            this.btn_reload_count_down.Click += new System.EventHandler(this.btn_reload_count_down_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 36);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = " 关机工具箱---V1.0---编译日期2023.2.6凌晨1：08";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox_select_time
+            // 
+            this.comboBox_select_time.Font = new System.Drawing.Font("楷体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox_select_time.FormattingEnabled = true;
+            this.comboBox_select_time.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "5",
+            "10",
+            "20",
+            "30",
+            "60",
+            "90",
+            "120",
+            "180",
+            "240"});
+            this.comboBox_select_time.Location = new System.Drawing.Point(527, 217);
+            this.comboBox_select_time.Name = "comboBox_select_time";
+            this.comboBox_select_time.Size = new System.Drawing.Size(328, 36);
+            this.comboBox_select_time.TabIndex = 7;
+            this.comboBox_select_time.Text = "选择倒计时(分钟)";
+            this.comboBox_select_time.SelectedIndexChanged += new System.EventHandler(this.comboBox_select_time_SelectedIndexChanged);
+            // 
+            // btn_sleep_1
+            // 
+            this.btn_sleep_1.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_sleep_1.Location = new System.Drawing.Point(71, 576);
+            this.btn_sleep_1.Name = "btn_sleep_1";
+            this.btn_sleep_1.Size = new System.Drawing.Size(120, 50);
+            this.btn_sleep_1.TabIndex = 8;
+            this.btn_sleep_1.Text = "睡眠";
+            this.btn_sleep_1.UseVisualStyleBackColor = true;
+            this.btn_sleep_1.Click += new System.EventHandler(this.btn_sleep_1_Click);
+            // 
+            // btn_sleep_1_count_down
+            // 
+            this.btn_sleep_1_count_down.Font = new System.Drawing.Font("黑体", 10.5F);
+            this.btn_sleep_1_count_down.Location = new System.Drawing.Point(264, 576);
+            this.btn_sleep_1_count_down.Name = "btn_sleep_1_count_down";
+            this.btn_sleep_1_count_down.Size = new System.Drawing.Size(210, 50);
+            this.btn_sleep_1_count_down.TabIndex = 9;
+            this.btn_sleep_1_count_down.Text = "倒计时睡眠";
+            this.btn_sleep_1_count_down.UseVisualStyleBackColor = true;
+            this.btn_sleep_1_count_down.Click += new System.EventHandler(this.btn_sleep_1_count_down_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_reset.Location = new System.Drawing.Point(618, 415);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(96, 77);
+            this.btn_reset.TabIndex = 10;
+            this.btn_reset.Text = "重置";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ShutdownTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 684);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_sleep_1_count_down);
+            this.Controls.Add(this.btn_sleep_1);
+            this.Controls.Add(this.comboBox_select_time);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_reload_count_down);
+            this.Controls.Add(this.btn_shutdown_count_down);
+            this.Controls.Add(this.btn_sleep_count_down);
+            this.Controls.Add(this.btn_reload);
+            this.Controls.Add(this.btn_shutdown);
+            this.Controls.Add(this.btn_sleep);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ShutdownTool";
+            this.Text = "Shutdown Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_sleep;
+        private System.Windows.Forms.Button btn_shutdown;
+        private System.Windows.Forms.Button btn_reload;
+        private System.Windows.Forms.Button btn_sleep_count_down;
+        private System.Windows.Forms.Button btn_shutdown_count_down;
+        private System.Windows.Forms.Button btn_reload_count_down;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox_select_time;
+        private System.Windows.Forms.Button btn_sleep_1;
+        private System.Windows.Forms.Button btn_sleep_1_count_down;
+        private System.Windows.Forms.Button btn_reset;
+    }
+}
+
---
+++ shutdown_tool/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+//添加命名空间
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+namespace shutdown_tool
+{
+    public partial class ShutdownTool : Form
+    {
+        //是否选择过时间
+        bool chooseTime = false;
+        //倒计时是否有效
+        bool countDown = true;
+        //倒计时时间
+        int countDownTime = 0;
+        //Delay函数调整参数，表示1分钟(有6万秒)
+        int delayArg = 1000 * 60;
+        //引入函数，实现休眠和睡眠功能
+        [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+        //电脑休眠
+        private void sleepFun()
+        {
+            Console.WriteLine("sleep.\n");
+            SetSuspendState(true, true, true);
+        }
+        //电脑关机
+        private void shutdownFun()
+        {
+            Console.WriteLine("shutdown.\n");
+            Process.Start("shutdown", "/s /t 0");
+        }
+        //电脑重启
+        private void reloadFun()
+        {
+            Console.WriteLine("reload.\n");
+            Process.Start("shutdown", "/r /t 0");
+        }
+        //电脑睡眠
+        private void sleepFun1()
+        {
+            Console.WriteLine("sleep1.\n");
+            SetSuspendState(false, true, true);
+        }
+        //毫秒延时 界面不会卡死
+        public bool Delay(int mm)
+        {
+            DateTime timeStart = DateTime.Now;
+            while (timeStart.AddMilliseconds(mm) > DateTime.Now)
+            {
+                Console.WriteLine(DateTime.Now);
+                this.comboBox_select_time.Text = ;
+                if (!this.countDown)
+                {
+                    return false;//未等待足够时间
+                }
+                Application.DoEvents();
+            }
+            return true;//等待了足够时间
+        }
+        private string DateDiff(DateTime DateTime1, DateTime DateTime2)
+        {
+            string dateDiff = null;
+            try
+            {
+                TimeSpan ts1 = new TimeSpan(DateTime1.Ticks);
+                TimeSpan ts2 = new TimeSpan(DateTime2.Ticks);
+                TimeSpan ts = ts1.Subtract(ts2).Duration();
+                dateDiff = dateDiff.ToString();
+                //dateDiff = ts.Days.ToString() + "天"
+               // + ts.Hours.ToString() + "小时"
+                //+ ts.Minutes.ToString() + "分钟"
+                //+ ts.Seconds.ToString() + "秒";
+            }
+            catch
+            { }
+            return dateDiff;
+        }
+
+        public ShutdownTool()
+        {
+            InitializeComponent();
+        }
+        private void comboBox_select_time_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.chooseTime = true;
+            int temp;
+            int.TryParse(comboBox_select_time.Text, out temp);
+            this.countDownTime = temp;
+            Console.WriteLine("获取输入：倒计时 = {0}\n", this.countDownTime);
+            this.comboBox_select_time.Enabled = false;
+        }
+
+        //btn_sleep
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.btn_sleep.Text = "已休眠";
+            this.btn_sleep.Enabled = false;
+            this.sleepFun();
+        }
+        //btn_shutdown
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.btn_shutdown.Text = "已关机";
+            this.btn_shutdown.Enabled = false;
+            this.shutdownFun();
+        }
+        //btn_reload
+        private void btn_reload_Click(object sender, EventArgs e)
+        {
+            this.btn_reload.Text = "已重启";
+            this.btn_reload.Enabled = false;
+            this.reloadFun();
+        }
+        //btn_sleep_count_down
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.btn_sleep_count_down.Text = "倒计时中...";
+            this.btn_sleep_count_down.Enabled = false;
+            countDown = true;
+
+            if (!chooseTime)
+            {
+                MessageBox.Show("请先选择倒计时的时间！");
+                this.btn_sleep_count_down.Enabled = true;
+                this.btn_sleep_count_down.Text = "倒计时休眠";
+            }
+            else
+            {
+                this.btn_sleep_1_count_down.Enabled = false;
+                this.btn_reload_count_down.Enabled = false;
+                this.btn_shutdown_count_down.Enabled = false;
+
+                if(Delay(this.countDownTime * delayArg))
+                {
+                    sleepFun();
+                }
+                
+            }
+        }
+        //btn_shutdown_count_down
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.btn_shutdown_count_down.Text = "倒计时中...";
+            this.btn_shutdown_count_down.Enabled = false;
+            countDown = true;
+
+            if (!chooseTime)
+            {
+                MessageBox.Show("请先选择倒计时的时间！");
+                this.btn_shutdown_count_down.Enabled = true;
+                this.btn_shutdown_count_down.Text = "倒计时关机";
+            }
+            else
+            {
+                this.btn_reload_count_down.Enabled = false;
+                this.btn_sleep_count_down.Enabled = false;
+                this.btn_sleep_1_count_down.Enabled = false;
+
+                if (Delay(this.countDownTime * delayArg))
+                {
+                    shutdownFun();
+                }
+
+            }
+        }
+
+        //btn_reload_count_down
+        private void btn_reload_count_down_Click(object sender, EventArgs e)
+        {
+            this.btn_reload_count_down.Text = "倒计时中...";
+            this.btn_reload_count_down.Enabled = false;
+            countDown = true;
+
+            if (!chooseTime)
+            {
+                MessageBox.Show("请先选择倒计时的时间！");
+                this.btn_reload_count_down.Enabled = true;
+                this.btn_reload_count_down.Text = "倒计时重启";
+            }
+            else
+            {
+                this.btn_sleep_1_count_down.Enabled = false;
+                this.btn_sleep_count_down.Enabled = false;
+                this.btn_shutdown_count_down.Enabled = false;
+
+                if (Delay(this.countDownTime * delayArg))
+                {
+                    reloadFun();
+                }
+            }
+        }
+        //重置
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.btn_sleep.Enabled = true;
+            this.btn_sleep.Text = "休眠";
+
+            //this.btn_sleep_1.Enabled = true;
+            //this.btn_sleep_1.Text = "睡眠";
+
+            this.btn_shutdown.Enabled = true;
+            this.btn_shutdown.Text = "关机";
+
+            this.btn_reload.Enabled = true;
+            this.btn_reload.Text = "重启";
+
+            countDown = false;
+            this.chooseTime = false;
+
+           this.btn_sleep_count_down.Enabled = true;
+            this.btn_sleep_count_down.Text = "倒计时休眠";
+
+            //this.btn_sleep_1_count_down.Enabled = true;
+            //this.btn_sleep_1_count_down.Text = "倒计时睡眠";
+
+            this.btn_shutdown_count_down.Enabled = true;
+            this.btn_shutdown_count_down.Text = "倒计时关机";
+
+            this.btn_reload_count_down.Enabled = true;
+            this.btn_reload_count_down.Text = "倒计时重启";
+
+            this.comboBox_select_time.Enabled = true;
+            this.comboBox_select_time.Text = "请重选时间";
+
+            this.countDownTime = 0;
+        }
+        //btn_sleep_1
+        private void btn_sleep_1_Click(object sender, EventArgs e)
+        {
+            this.btn_sleep_1.Text = "已睡眠";
+            this.btn_sleep_1.Enabled = false;
+            this.sleepFun1();
+        }
+        //btn_sleep_1_count_down
+        private void btn_sleep_1_count_down_Click(object sender, EventArgs e)
+        {
+            this.btn_sleep_1_count_down.Text = "倒计时睡眠中...";
+            this.btn_sleep_1_count_down.Enabled = false;
+
+            if (!chooseTime)
+            {
+                MessageBox.Show("请先选择倒计时的时间！");
+                this.btn_sleep_1_count_down.Enabled = true;
+            }
+            else
+            {
+                this.btn_reload_count_down.Enabled = false;
+                this.btn_sleep_count_down.Enabled = false;
+                this.btn_shutdown_count_down.Enabled = false;
+
+                Delay(this.countDownTime * delayArg);
+                sleepFun1();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btn_sleep_1.Enabled = false;
+            this.btn_sleep_1_count_down.Enabled = false;
+            this.textBox1.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 终止此进程，并将退出代码返回到操作系统。将强制退出所有正在运行的线程，彻底退出。
+            System.Environment.Exit(0);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
